d768af71724db3d0c14a84af747d0477b97865c0
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-21 10:36:43 -0700

    Sounds play on start

--- app/src/main/java/com/example/clarkhinchcliff/soundtest/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/soundtest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.clarkhinchcliff.soundtest;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaPlayer mPlayer = MediaPlayer.create(this, R.raw.toot);
+
+        mPlayer.start();
     }
 }
