0ba4cadfe397959adcf72fa3dfc4709514e800da
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-22 20:55:10 -0700

    Can pause and play

--- app/src/main/java/com/example/clarkhinchcliff/soundtest/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/soundtest/MainActivity.java
@@ -3,16 +3,24 @@ package com.example.clarkhinchcliff.soundtest;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mPlayer;
+
+    public void playAudio(View view) {
+        mPlayer.start();
+    }
+
+    public void pausePause(View view) {
+        mPlayer.pause();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MediaPlayer mPlayer = MediaPlayer.create(this, R.raw.toot);
-
-        mPlayer.start();
+        mPlayer = MediaPlayer.create(this, R.raw.toot);
     }
 }
