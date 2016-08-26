5deae2dba11f26b6bec00e056d8e7bd6914b9b42
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-25 17:31:39 -0700

    volume control started

--- app/src/main/java/com/example/clarkhinchcliff/soundtest/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/soundtest/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.clarkhinchcliff.soundtest;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mPlayer = MediaPlayer.create(this, R.raw.toot);
+        SeekBar volumeControl = (SeekBar) findViewById(R.id.seekBar);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            Log.i("I am your father");
+
+        });
     }
 }
